42f33245e0f291adea6178cf0cf03835f79a49ec
c0deblack <79178809+c0deblack@users.noreply.github.com>
2022-12-14 23:59:34 -0500

    Chapter 09

---
+++ code/app/src/main/java/com/c0deblack/bignerdranch/ch09/CriminalIntent.kt
@@ -0,0 +1,11 @@
+package com.c0deblack.bignerdranch.ch09
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CriminalIntent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_criminal_intent)
+    }
+}
